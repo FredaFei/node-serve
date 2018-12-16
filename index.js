d7ee336bea988954fd108e8735162bb94431bd59
fei-han <964959033@qq.com>
2018-12-16 22:01:35 +0800

    node service

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const upload = multer({dest: 'uploads/'})
+
+const app = new express()
+app.get('/',(req,res)=>{
+  res.send('hello service')
+})
+app.options('/upload', cors());
+app.post('/upload',cors(), upload.single('file'), (req,res)=>{
+  res.send(req.file.filename)
+})
+
+
+app.get('/preview/:key',cors(), (req,res)=>{
+  res.sendFile(
+    `uploads/${req.params.key}`,
+    {
+      root: __dirname,
+      headers: {
+        'Content-Type': 'image/jpeg',
+      }
+    },
+    error=>{
+      console.log(error)
+    }
+  )
+})
+const port = process.env.PORT || 3000
+
+app.listen(port)
